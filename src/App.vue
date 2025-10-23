2996ca9b025391818d0fcbe24abd10f8027f2b58
Matthew King <thewmking@gmail.com>
2025-10-23 16:21:54 -0400

    Add ref

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import CellGrid from "./components/CellGrid.vue";
 import ColorPicker from "./components/ColorPicker.vue";
 
